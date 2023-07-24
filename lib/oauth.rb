a909914b7074c575d6b1c1f8873fa215e0b5bd4e
Masahiro Morita <m.morita@getgamba.com>
2023-07-24 16:25:13 +0800

    Add RSA-SA256 signature support

--- lib/oauth.rb
+++ lib/oauth.rb
@@ -13,6 +13,7 @@ require "oauth/signature/plaintext"
 require "oauth/signature/hmac/sha1"
 require "oauth/signature/hmac/sha256"
 require "oauth/signature/rsa/sha1"
+require "oauth/signature/rsa/sha256"
 require "oauth/request_proxy/mock_request"
 
 OAuth::Version.class_eval do
